dfad4f1aa7f31d6f137265a631d1c0ea143b99d5
Matthew Podolak <mpodola2@gmail.com>
2022-12-23 15:09:12 -0500

    fix: typo

--- tests/test_response.py
+++ tests/test_response.py
@@ -5,18 +5,18 @@ from pmaw import Response, PushshiftAPI
 
 # TODO: add submission_comment_ids tests once endpoint is working again
 # expected_length = 66
-excepted_length = 0
+expected_length = 0
 
 @tape.use_cassette('test_submission_comment_ids_search')
 def test_response_load_cache():
   api = PushshiftAPI(file_checkpoint=1)
   comments = api.search_submission_comment_ids(ids=post_ids, mem_safe=True)
   resp = Response.load_cache(key=comments._cache.key)
-  assert(len(comments) == len(resp) and len(comments) == excepted_length)
+  assert(len(comments) == len(resp) and len(comments) == expected_length)
 
 @tape.use_cassette('test_submission_comment_ids_search')
 def test_response_generator():
   api = PushshiftAPI(file_checkpoint=1)
   comments = api.search_submission_comment_ids(ids=post_ids, mem_safe=True)
   all_c = [c for c in comments]
-  assert(len(all_c) == excepted_length)
+  assert(len(all_c) == expected_length)
--- tests/test_search_submission_comment_ids.py
+++ tests/test_search_submission_comment_ids.py
@@ -4,28 +4,28 @@ from pmaw import PushshiftAPI
 
 # TODO: update submission_comment_ids tests once endpoint is working again
 # expected_length = 66
-excepted_length = 0
+expected_length = 0
 
 @tape.use_cassette()
 def test_submission_comment_ids_search():
   api = PushshiftAPI(file_checkpoint=1)
   comments = api.search_submission_comment_ids(ids=ids)
-  assert(len(comments) == excepted_length)
+  assert(len(comments) == expected_length)
 
 @tape.use_cassette()
 def test_submission_comment_ids_praw():
   api_praw = PushshiftAPI(file_checkpoint=1, praw=reddit)
   comments = api_praw.search_submission_comment_ids(ids=ids)
-  assert(len(comments) == excepted_length)
+  assert(len(comments) == expected_length)
 
 @tape.use_cassette('test_submission_comment_ids_search')
 def test_submission_comment_ids_search_mem_safe():
   api = PushshiftAPI(file_checkpoint=1)
   comments = api.search_submission_comment_ids(ids=ids, mem_safe=True)
-  assert(len(comments) == excepted_length)
+  assert(len(comments) == expected_length)
 
 @tape.use_cassette('test_submission_comment_ids_praw')
 def test_submission_comment_ids_praw_mem_safe():
   api_praw = PushshiftAPI(file_checkpoint=1, praw=reddit)
   comments = api_praw.search_submission_comment_ids(ids=ids, mem_safe=True)
-  assert(len(comments) == excepted_length)
+  assert(len(comments) == expected_length)
